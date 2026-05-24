c92953ec73fa2460ce5252dcbb2f8d1487325b50
Sakura520222 <sakura520222@outlook.com>
2026-05-24 21:41:13 +0800

    feat(aside): Add analytics menu item to sidebar navigation
    - Add new menu item for analytics route with click handler to navigate to 'analysis'
    - Include fluent:data-pie-20-regular icon and localized text for 'analytics'
    - Apply conditional styling based on current route's meta name

--- mail-vue/src/layout/aside/index.vue
+++ mail-vue/src/layout/aside/index.vue
@@ -26,6 +26,11 @@
           <Icon icon="solar:star-line-duotone" width="20" height="20" />
           <span class="menu-name" style="margin-left: 21px">{{$t('starred')}}</span>
         </el-menu-item>
+        <el-menu-item @click="router.push({name: 'analysis'})" index="analysis"
+                      :class="route.meta.name === 'analysis' ? 'choose-item' : ''">
+          <Icon icon="fluent:data-pie-20-regular" width="20" height="20" />
+          <span class="menu-name" style="margin-left: 21px">{{$t('analytics')}}</span>
+        </el-menu-item>
         <el-menu-item @click="router.push({name: 'setting'})" index="setting"
                       :class="route.meta.name === 'setting' ? 'choose-item' : ''">
           <Icon icon="fluent:settings-48-regular" width="20" height="20" />
